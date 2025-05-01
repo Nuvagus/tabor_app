e71f6efc449759aca582605e167065f23fe35572
Nuvagus <81617488+Nuvagus@users.noreply.github.com>
2025-05-01 15:19:05 +0200

    Add files via upload

---
+++ tabori_stopper_pwa/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('stopper-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
